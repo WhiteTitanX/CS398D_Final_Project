38226d4c68560dbe7d7d2de915f2f752e1f9e5fc
Billy Gerhard <billygerhard@gmail.com>
2019-10-15 15:04:50 -0400

    Updated Chatroom with new layout. Colors just placeholder. Can update with global scheme later.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # CS398D_Final_Project
-
 1. Login Page - Jonathan
 2. Sign Up Page - Jonathan
     - Chatroom List - Alex
         - Actual Chat - Billy
     - Contacts - Marta
+
+
+##Build Notes:
+###React Native Elements
+1. npm i react-native-vector-icons --save
+2. react-native link react-native-vector-icons
+3. npm i react-native-elements --save
+
+
--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -8,26 +8,77 @@ import {
   Text,
   TouchableOpacity,
   View,
+  TextInput,
 } from 'react-native';
-
 import { MonoText } from '../components/StyledText';
+import Colors from "../constants/Colors";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function ChatRoomScreen() {
   return (
     <View style={styles.container}>
+      <View style={[styles.headerContainer,styles.headerStyle]}>
+        <Text style={styles.headerText}>
+          Chat With Chad
+        </Text>
+      </View>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
 
-        <Text>
-          Chatroom
-        </Text>
+        {/* Your Message */}
+        <View style={styles.viewWrapItemRight}>
+          <Text style={styles.textItemRight}>How are you today?</Text>
+        </View>
+
+        {/* Their Message field */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image
+            style={styles.avatarItemLeft}
+            source={{uri: "https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"}}
+          />
+          <View style={styles.viewWrapItemLeft}>
+            <Text style={styles.textItemLeft}>I'm doing great!</Text>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image
+            style={styles.avatarItemLeft}
+            source={{uri: "https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"}}
+          />
+          <View style={styles.viewWrapItemLeft}>
+            <Text style={styles.textItemLeft}>Do you want to go to the library?</Text>
+          </View>
+        </View>
+
+        {/* Your Message */}
+        <View style={styles.viewWrapItemRight}>
+          <Text style={styles.textItemRight}>That sounds perfect!</Text>
+        </View>
+
       </ScrollView>
 
       <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          Footer Bar
-        </Text>
+        <View style={styles.viewWrapInput}>
+          {/* Input field */}
+          <TextInput
+            underlineColorAndroid="rgba(0,0,0,0)"
+            style={styles.viewTextInput}
+            placeholder="Type your message..."
+            onChangeText={value => {
+              //Do work here to send message.
+            }}
+            //value={this.state.currentMessage}
+          />
+
+          {/* Button send message */}
+          <TouchableOpacity
+            //onPress={this.sendMessage}
+          >
+            <Ionicons name="md-send" size={32} color="#488aff" />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -40,7 +91,7 @@ ChatRoomScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: '#fff',
   },
   developmentModeText: {
     marginBottom: 20,
@@ -85,6 +136,11 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     textAlign: 'center',
   },
+  headerBarContainer: {
+    alignItems: 'center',
+    backgroundColor: 'cream',
+    paddingVertical: 0,
+  },
   tabBarInfoContainer: {
     position: 'absolute',
     bottom: 0,
@@ -103,7 +159,7 @@ const styles = StyleSheet.create({
     }),
     alignItems: 'center',
     backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    paddingVertical: 0,
   },
   tabBarInfoText: {
     fontSize: 17,
@@ -124,4 +180,79 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
+  viewWrapInput: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 50,
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 10
+  },
+  viewTextInput: {
+    flex: 1,
+  },
+
+  // Message right
+  viewWrapItemRight: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
+    marginBottom: 6,
+    marginTop: 6
+  },
+  textItemRight: {
+    borderRadius: 10,
+    width: 170,
+    backgroundColor: 'lightskyblue',
+    color: 'black',
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 10,
+    paddingRight: 10,
+    overflow: 'hidden'
+  },
+
+  // Message left
+  viewWrapItemLeft: {
+    marginLeft: 10,
+    marginBottom: 6,
+    marginTop: 6,
+  },
+  textItemLeft: {
+    borderRadius: 10,
+    width: 170,
+    backgroundColor: '#203152',
+    color: 'white',
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 10,
+    paddingRight: 10,
+    overflow: 'hidden'
+  },
+  avatarItemLeft: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginLeft: 10
+  },
+  // my own styles for titleAndIcon
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingLeft: 8,
+  },
+  headerStyle: {
+    backgroundColor: '#455a64',
+  },
+  // my own styles for titleAndIcon
+  headerText: {
+    paddingLeft: 8,
+    color: '#d9d9d9',
+    fontFamily: 'NotoSans-Regular',
+    fontSize: 20,
+    textAlign: 'center',
+    width: '100%'
+  }
+
 });
