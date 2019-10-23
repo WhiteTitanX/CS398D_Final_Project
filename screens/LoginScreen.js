10f5e236aeca9fcd0e3777b6a2d1f5de990048f4
AC <Alex105riga@gmail.com>
2019-10-22 22:00:46 -0400

    Updated LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,127 +1,183 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+  AppRegistry, Text, View, StyleSheet, ImageBackground, TextInput, Image, TouchableHighlight
+} 
+from 'react-native';
 
 import { MonoText } from '../components/StyledText';
+import { Constants } from 'expo';
+export default class App extends Component {
+    state = {
+        firstname: '',
+        username: '',
+        password: '',
+        phonenumber: '',
+        lastname: '',
+        
+    };
 
-export default function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-
-        <Text>
-          Login
-        </Text>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          Footer Bar
-        </Text>
-      </View>
-    </View>
-  );
+    _handleTextChange = inputValue => {
+        this.setState({ inputValue });
+    
+};
+    render() {
+        return (
+            
+            <View style={styles.container}>
+            
+                <ImageBackground
+                    style={styles.background}
+                    source={{ uri: 'https://codehs.com/uploads/802894298a13da568102a68e0aa4098f' }}
+                >
+        
+                <View style={styles.top}>
+                    <Text style={styles.paragraph}>
+                    YOU ARE ALMOST THERE!
+                    
+                    </Text>
+                     </View>
+                     
+                     <Image
+                         source={{ uri: 'https://codehs.com/uploads/a0040bc1d6f8418011ce8f239068af3d' }}
+                         style={{ height: 80, width: 300 }}
+                     />
+                 
+                    <View style={styles.middle}>
+                    <Text style={styles.paragraph}>
+                    First Name:
+                    
+                    </Text>
+                    
+                    < TextInput style={styles.firstnameInput}
+                onChangeText={(firstname) => this.setState({firstname})}
+                value={this.state.firstname}
+                        style={{ width: 250, height: 44, padding: 8, backgroundColor: 'grey', borderRadius: 20, marginLeft: 20, color: 'white'}}
+                    />
+                    <Text style={styles.paragraph}>
+                    Last Name:
+                    
+                    </Text>
+                    
+                    <TextInput style={styles.lastnameInput}
+                onChangeText={(lastname) => this.setState({lastname})}
+                value={this.state.lastname}
+                        style={{ width: 250, height: 44, padding: 8, backgroundColor: 'grey', borderRadius: 20, marginLeft: 20, color: 'white'}}
+                    />
+                    <Text style={styles.paragraph}>
+                    UserName:
+                    
+                    </Text>
+                    
+                    <TextInput style={styles.usernameInput}
+                onChangeText={(username) => this.setState({username})}
+                value={this.state.username}
+                        style={{ width: 250, height: 44, padding: 8, backgroundColor: 'grey', borderRadius: 20, marginLeft: 20, color: 'white'}}
+                    />
+                    <Text style={styles.paragraph}>
+                    Password:
+                    
+                    </Text>
+                    
+                    <TextInput style={styles.passwordInput}
+                onChangeText={(password) => this.setState({password})}
+                value={this.state.password}
+                        style={{ width: 250, height: 44, padding: 8, backgroundColor: 'grey', borderRadius: 20, marginLeft: 20, color: 'white'}}
+                    />
+                    <Text style={styles.paragraph}>
+                    Phone Number:
+                    
+                    </Text>
+                    
+                    <TextInput style={styles.phonenumberInput}
+                onChangeText={(phonenumber) => this.setState({phonenumber})}
+                value={this.state.phonenumber}
+                        style={{ width: 250, height: 44, padding: 8, backgroundColor: 'grey', borderRadius: 20, marginLeft: 20, color: 'white'}}
+                    />
+                    
+                    </View>
+                    
+                    <View style={styles.bottom}>
+                    
+                    
+                        
+                        <TouchableHighlight
+                            onPress={() => {
+                                alert('You Have Signed Up Successfully!')
+                            }}
+                        >
+                        
+                            <View style={styles.box}>
+                            
+                                <Text style={styles.text}>
+                                SIGN UP
+                                </Text>
+                            
+                            </View>
+                        
+                        </TouchableHighlight>
+                    
+                    
+                    
+                    </View>
+    
+                </ImageBackground>
+                
+            </View>
+        );
+    }
 }
 
-LoginScreen.navigationOptions = {
-  header: null,
-};
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  LoginScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    container: {
+        flex: 1,
+      
+    
+      
+    },
+    top: {
+     flex: 1,
+        width: 300,
+        
+    },
+    middle: {
+        flex: 3,
+        width: 300,
+        
+    },
+    background: {
+        width: 300,
+        height: 540,
+        
+    },
+    paragraph: {
+        fontSize: 25,
+        fontWeight: 'bold',
+     
+        color: 'white',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+       
+        color: 'white',
+    },
+    bottom: {
+        
+       flex: 1,
+         alignItems: 'center',
+       
+         justifyContent: 'center',
+        alignItems: 'center',
+    },
+    box: {
+        height: 44,
+        width: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#307172',
+        borderRadius: 20,
+      
+        
+    }
 });
